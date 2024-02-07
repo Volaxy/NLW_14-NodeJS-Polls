d515171420c92e15bea8889a2b7d385330f4214d
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2024-02-07 12:31:06 -0300

    ADD - Create Poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,26 @@
 import fastify from "fastify";
+import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify();
 
-app.post("/polls", (request) => {
-    console.log(request.body);
+const prisma = new PrismaClient();
+
+app.post("/polls", async (request, response) => {
+    // O "zod" define a estrutura que o objeto deve ter quando for enviada uma requisição
+    const createPollBody = z.object({
+        title: z.string()
+    });
+
+    const { title } = createPollBody.parse(request.body);
+
+    const poll = await prisma.poll.create({
+        data: {
+            title,
+        }
+    });
+
+    return response.status(201).send(poll);
 });
 
 app.listen({ port: 3333 }).then(() => {
